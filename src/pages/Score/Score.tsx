98ee157f754caa42cfcab5d64f82d52f127a312d
Eugene <gradam2001@gmail.com>
2023-05-11 00:58:18 +0300

    feat: add headers to score table

--- src/pages/Score/Score.tsx
+++ src/pages/Score/Score.tsx
@@ -30,15 +30,21 @@ const Score = () => {
     <PageContainer title="Score">
       {score.length ? (
         <Card>
+          <ul className={classes.headers__list}>
+            <li className={classes['headers__list-number']}>№</li>
+            <li className={classes['headers__list-points']}>Points</li>
+            <li className={classes['headers__list-mode']}>Mode</li>
+            <li className={classes['headers__list-date']}>Date</li>
+          </ul>
           <ul className={classes.score__list}>
             {scoreCopy
               .sort((a, b) => b.points - a.points)
               .map((item, i) => (
                 <li key={i} className={classes['score__list-item']}>
-                  <p>{i + 1}</p>
-                  <p>{item.points}</p>
-                  <p>{item.mode}</p>
-                  <p>{parseDate(item.date)}</p>
+                  <p className={classes['score__list-item-number']}>{i + 1}</p>
+                  <p className={classes['score__list-item-points']}>{item.points}</p>
+                  <p className={classes['score__list-item-mode']}>{item.mode}</p>
+                  <p className={classes['score__list-item-date']}>{parseDate(item.date)}</p>
                 </li>
               ))}
           </ul>
