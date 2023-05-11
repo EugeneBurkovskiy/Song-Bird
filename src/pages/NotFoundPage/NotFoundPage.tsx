c260a578eed164c3b550aa8a557b3d13300ca530
Eugene <gradam2001@gmail.com>
2023-05-11 19:42:03 +0300

    feat: add 404 page layout, redirect to main page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router';
+
+import classes from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
-  return <div>404</div>;
+  const [redirect, setRedirect] = useState(false);
+
+  setTimeout(() => setRedirect(!redirect), 2000);
+
+  return redirect ? <Navigate to="/" replace /> : <section className={classes.error}>404</section>;
 };
 
 export default NotFoundPage;
